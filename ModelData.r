baad1f6049cd7824bfc0c966f783d2c012c2b86d
g.v.s.s pavan prakash <gvsspavanprakash@gmail.com>
2017-08-21 18:16:38 +0530

    Update and rename Loop WorksheetR.r to ModelData.r

--- ModelData.r
+++ ModelData.r
@@ -1,7 +1,11 @@
 install.packages("dplyr")
 install.packages("openxlsx")
+install.packages("xlsx")
+install.packages("rJava")
 library("openxlsx")
 library("dplyr")
+library("rJava")
+library("xlsx")
 #get Sheetnames of a excelworkbook
 Sheetnames<- getSheetNames("F:\\Demand forecasting\\Original data\\DMBI.xlsx")
 #get no of sheets in a excelworkbook
@@ -18,11 +22,11 @@ Materialloop<-length(Material)
 for(i in 1:Materialloop){
   tempMaterial <- Material[i]
   tempdata<-data.frame()
-  #print(paste("current Material in loop ",tempMaterial))
-  #print(paste("Material number is",i))
+  print(paste("current Material in loop ",tempMaterial))
+  print(paste("Material number is",i))
   for(j in 1:Sheetloop){
     dataframe1<-read.xlsx("F:\\Demand forecasting\\Original data\\DMBI.xlsx",sheet=j,cols=c(1,2,3,10,11))
-    #print(paste("Sheet number is value is",j))
+    print(paste("Sheet number is value is",j))
     #apply filter to a dataframe
     Filtereddata<-filter(dataframe1,Material==tempMaterial)
     Salesvalue <- sum(Filtereddata$Umsatz)
@@ -31,6 +35,7 @@ for(i in 1:Materialloop){
     tempdata[j,5]<-Salesvalue
   }
   finaldata<-rbind(finaldata,tempdata)
-  #print(finaldata)
+  print(finaldata)
 }
-
+write.xlsx(x = finaldata, file = "F:\\Demand forecasting\\Original data\\MOdelData.xlsx",col.names = TRUE,
+           sheetName = "TestSheet", row.names = FALSE)
